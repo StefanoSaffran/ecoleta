68e18dea72633e70f0352ff8304199fc1398827f
Stefano Saffran <stefanosaffran@hotmail.com>
2020-06-06 01:46:44 +0200

    feat: add Toast component

---
+++ frontend/src/components/ToastContainer/Toast/styles.ts
@@ -0,0 +1,74 @@
+import styled, { css } from 'styled-components';
+import { animated } from 'react-spring';
+
+interface IContainerProps {
+  type?: 'success' | 'error' | 'info';
+  hasDescription: number;
+}
+
+const toastTypeVariations = {
+  info: css`
+    color: #3172b7;
+    background: #ebf8ff;
+  `,
+  success: css`
+    color: #2e656a;
+    background: #e6fffa;
+  `,
+  error: css`
+    color: #c53030;
+    background: #fddede;
+  `,
+};
+
+export const Container = styled(animated.div)<IContainerProps>`
+  position: relative;
+  width: 360px;
+  padding: 16px 30px 16px 16px;
+  border-radius: 10px;
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
+  display: flex;
+
+  ${({ type }) => toastTypeVariations[type || 'info']}
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  > svg {
+    margin: 4px 12px 0 0;
+  }
+
+  div {
+    flex: 1;
+
+    strong {
+      color: #322153;
+    }
+    p {
+      margin-top: 4px;
+      font-size: 14px;
+      opacity: 0.8;
+      line-height: 20px;
+      color: #6c6c80;
+    }
+  }
+
+  button {
+    position: absolute;
+    top: 19px;
+    right: 16px;
+    border: 0;
+    background: transparent;
+    color: inherit;
+  }
+
+  ${({ hasDescription }) =>
+    !hasDescription &&
+    css`
+      align-items: center;
+      svg {
+        margin-top: 0;
+      }
+    `}
+`;
